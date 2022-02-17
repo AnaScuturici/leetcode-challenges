d4c5aa0b14f31c629f8d6cd603b0425ad609cff2
ana scuturici <anascuturici@gmail.com>
2022-02-17 14:27:49 +0900

    add: single loop solution for twoSum

--- Two-sum/two-sum.js
+++ Two-sum/two-sum.js
@@ -6,13 +6,29 @@
 
 // O(n2) time complexity
 
- const twoSum = function(nums, target) {
+//  const twoSum = function(nums, target) {
+//     for(let i = 0; i < nums.length; i++){
+//         for(let j = i + 1; j < nums.length; j++){
+//             if(nums[i] + nums[j] === target){
+//                 return [i, j];
+//             }
+//         }
+//     }
+// };
+
+// O(n) time complexity
+const twoSum = function(nums, target) {
+    let result = {};
     for(let i = 0; i < nums.length; i++){
-        for(let j = i + 1; j < nums.length; j++){
-            if(nums[i] + nums[j] === target){
-                return [i, j];
-            }
+        let currVal = nums[i];
+        let targetVal = target - currVal;
+        let i2 = result[targetVal];
+        if(i2 != null){
+            return [i2, i];
+        } else {
+            result[currVal] = i;
         }
     }
 };
 
+module.exports = {  twoSum };
