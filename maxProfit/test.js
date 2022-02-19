ea9d62ab503e780c8d6ed2803e97e67e0fa2fe9e
ana scuturici <anascuturici@gmail.com>
2022-02-19 23:56:01 +0900

    fix: correct test maxProfit

--- maxProfit/test.js
+++ maxProfit/test.js
@@ -5,6 +5,6 @@ describe.only("maxProfit", () => {
   it("Function produces expected results with valid input.", () => {
     expect(maxProfit([7,1,5,3,6,4])).to.equal(5);
     expect(maxProfit([7,6,4,3,1])).to.equal(0);
-    expect(maxProfit([2,4,1])).to.equal(0);
+    expect(maxProfit([2,4,1])).to.equal(2);
      });
 });
