e091201c3da4e1a654e8e16afc0b17f7f41588e6
ana scuturici <anascuturici@gmail.com>
2022-02-20 23:10:31 +0900

    add: maxSubarray

---
+++ 4. maxSubarray/maxSubarray.js
@@ -0,0 +1,13 @@
+/**
+Given an integer array nums, find the contiguous subarray (containing at least one number) 
+which has the largest sum and return its sum.
+A subarray is a contiguous part of an array.
+
+ * @param {number[]} nums
+ * @return {number}
+ */
+ const maxSubarray = (nums) => {
+    
+};
+
+module.exports = maxSubarray;
---
+++ 4. maxSubarray/test.js
@@ -0,0 +1,10 @@
+const { expect } = require("chai");
+const { maxSubarray } = require("./maxSubarray.js");
+
+describe.only("maxSubarray", () => {
+  it("Function produces expected results with valid input.", () => {
+    expect(maxSubarray([-2,1,-3,4,-1,2,1,-5,4])).to.equal(6);
+    expect(maxSubarray([1])).to.equal(1);
+    expect(maxSubarray([5,4,-1,7,8])).to.equal(23);
+     });
+});
