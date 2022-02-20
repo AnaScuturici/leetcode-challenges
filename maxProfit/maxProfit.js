2845b9a812d0607d495b7d6ff8db53e20daf5780
ana scuturici <anascuturici@gmail.com>
2022-02-20 22:39:46 +0900

    fix: change solution for maxproft to max difference between 2 array values

--- maxProfit/maxProfit.js
+++ maxProfit/maxProfit.js
@@ -3,12 +3,19 @@
  * @return {number}
  */
  const maxProfit = (prices) => {
-    const buy = Math.min(...prices);
-    const sliced = prices.slice(prices.indexOf(buy));
-    const sell = Math.max(...sliced);
-    if(prices.indexOf(buy) === prices.length - 1){
-        return 0;
-    } else return sell - buy;
+     // find highest num = difference between 2 nums
+     // as long as the lower num is before the higher
+    let min = prices[0]; // assume lower num is at lower index
+    let maxProfit = 0; // initialize profit at 0 (i.e. no profit)
+    for(let i = 0; i < prices.length; i++){
+        if(prices[i] < min){
+            min = prices[i]; // update lower num
+        }
+        if(prices[i] > min && maxProfit < prices[i] - min){ 
+            maxProfit = prices[i] - min; // update profit by deducting the lower num out of the higher one
+        }
+    }
+    return maxProfit;
 };
 
 module.exports = { maxProfit };
