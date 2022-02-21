4028a4dcdebb48e791b50d13a18077b02645eb34
ana scuturici <anascuturici@gmail.com>
2022-02-21 21:57:34 +0900

    add: solution for maxSubarray

--- 4.maxSubarray/maxSubarray.js
+++ 4.maxSubarray/maxSubarray.js
@@ -6,8 +6,17 @@ A subarray is a contiguous part of an array.
  * @param {number[]} nums
  * @return {number}
  */
+
  const maxSubarray = (nums) => {
-    
+    let maxSum = nums[0];
+    let currSum = nums[0];
+
+    for(let i = 1; i < nums.length; i++){
+        currSum = Math.max(nums[i], nums[i] + currSum);
+        maxSum = Math.max(currSum, maxSum);
+    }
+
+    return maxSum;
 };
 
-module.exports = maxSubarray;
+module.exports = { maxSubarray };
