bdfb0940b7ae25d486f2d21d13f18650ef332739
ana scuturici <anascuturici@gmail.com>
2022-02-21 21:56:05 +0900

    fix: space and typo in 3.containsDuplicates

---
+++ 3.containsDuplicates/containsDuplicates.js
@@ -0,0 +1,13 @@
+/**
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+
+const containsDuplicates = (nums) => {
+    const unique = new Set(nums);
+    return nums.length !== [...unique].length; 
+};
+
+module.exports = containsDuplicates;
