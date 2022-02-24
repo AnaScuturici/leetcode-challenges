d27aa9ded049c7b2e687618b66f00753bce4b622
ana scuturici <anascuturici@gmail.com>
2022-02-24 21:59:28 +0900

    add: comments for getSum recursive

--- 5.getSum/getSum.js
+++ 5.getSum/getSum.js
@@ -12,4 +12,12 @@
     return getSum(a ^ b, (a & b) << 1);
 };
 
+/*
+2 in binary = 10;
+3 in binary = 11;
+
+2 ^ 3 = 01; // in binary => 1
+(2 & 3) << 1 = 100; // in binary => 4 (2 & 3 = 10, shift by 1 bit to the left => 100)
+*/
+
 module.exports = { getSum };
