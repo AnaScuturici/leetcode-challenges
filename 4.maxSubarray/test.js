e15d230769dd74cf4d672d0c859e0aa485742cdd
ana scuturici <anascuturici@gmail.com>
2022-02-24 20:54:28 +0900

    delete: .only in test file

--- 4.maxSubarray/test.js
+++ 4.maxSubarray/test.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { maxSubarray } = require("./maxSubarray.js");
 
-describe.only("maxSubarray", () => {
+describe("maxSubarray", () => {
   it("Function produces expected results with valid input.", () => {
     expect(maxSubarray([-2, 1, -3, 4, -1, 2, 1, -5, 4])).to.equal(6);
     expect(maxSubarray([1])).to.equal(1);
