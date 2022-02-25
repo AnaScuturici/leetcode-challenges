33374c75c202aa454d15db9399a779b4d8ea53c4
ana scuturici <anascuturici@gmail.com>
2022-02-25 21:05:44 +0900

    fix: getSum test

--- 5.getSum/test.js
+++ 5.getSum/test.js
@@ -1,10 +1,10 @@
 const { expect } = require("chai");
 const { getSum } = require("./getSum.js");
 
-describe.only("getSum", () => {
+describe("getSum", () => {
   it("Function produces expected results with valid input.", () => {
-    expect(getSum(1, 2)).to.deep.equal(3);
-    expect(getSum(2, 3)).to.deep.equal(5);
-    expect(getSum(-2, 3)).to.deep.equal(1);
+    expect(getSum(1, 2)).to.equal(3);
+    expect(getSum(2, 3)).to.equal(5);
+    expect(getSum(-2, 3)).to.equal(1);
      });
 });
