b2f0ca5a47f1622df1c0aae4bf50b6ed21c9a8e8
ana scuturici <anascuturici@gmail.com>
2022-02-27 22:32:44 +0900

    fix: fix tests to input = binary string

--- 6.hammingWeight/test.js
+++ 6.hammingWeight/test.js
@@ -2,9 +2,10 @@ const { expect } = require("chai");
 const { hammingWeight } = require("./hammingWeight.js");
 
 describe.only("hammingWeight", () => {
+    // amended test to input = string (input binary string)
   it("Function produces expected results with valid input.", () => {
-    expect(hammingWeight(00000000000000000000000000001011)).to.equal(3);
-    expect(hammingWeight(00000000000000000000000010000000)).to.equal(1);
-    expect(hammingWeight(11111111111111111111111111111101)).to.equal(31);
+    expect(hammingWeight('00000000000000000000000000001011')).to.equal(3);
+    expect(hammingWeight('00000000000000000000000010000000')).to.equal(1);
+    expect(hammingWeight('11111111111111111111111111111101')).to.equal(31);
      });
 });
