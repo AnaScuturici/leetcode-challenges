6b65d72e10a6898c205aa6f223c7a682d8dad221
ana scuturici <anascuturici@gmail.com>
2022-02-27 22:36:09 +0900

    refactor: one-liner solution

--- 6.hammingWeight/hammingWeight.js
+++ 6.hammingWeight/hammingWeight.js
@@ -5,13 +5,7 @@ Write a function that takes an unsigned integer and returns the number of '1' bi
  * @return {number}
  */
  const hammingWeight = n => {
-    let count = 0;
-    let array = n.toString(2);
-
-    for(let num of array){
-        if(num == 1) count++;
-    }
-    return count;
+    return n.toString(2).split(1).length - 1;
 };
 
 module.exports = { hammingWeight };
