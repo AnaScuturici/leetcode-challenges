d6a3d2e6f37d448e2d4b026b725e962c917069bb
ana scuturici <anascuturici@gmail.com>
2022-02-27 22:44:23 +0900

    add: missingNumber requirements

---
+++ 7.missingNumber/test.js
@@ -0,0 +1,10 @@
+const { expect } = require("chai");
+const { missingNumber } = require("./missingNumber.js");
+
+describe.only("missingNumber", () => {
+  it("Function produces expected results with valid input.", () => {
+    expect(missingNumber([3,0,1])).to.equal(2);
+    expect(missingNumber([0,1])).to.equal(2);
+    expect(missingNumber([9,6,4,2,3,5,7,0,1])).to.equal(8);
+     });
+});
