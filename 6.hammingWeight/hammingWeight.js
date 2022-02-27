e9a470f29b3f634600a28a37d501d98cab533011
ana scuturici <anascuturici@gmail.com>
2022-02-27 22:34:51 +0900

    add: for loop solution

--- 6.hammingWeight/hammingWeight.js
+++ 6.hammingWeight/hammingWeight.js
@@ -4,8 +4,14 @@ Write a function that takes an unsigned integer and returns the number of '1' bi
  * @param {number} n - a positive integer
  * @return {number}
  */
- var hammingWeight = function(n) {
-    
+ const hammingWeight = n => {
+    let count = 0;
+    let array = n.toString(2);
+
+    for(let num of array){
+        if(num == 1) count++;
+    }
+    return count;
 };
 
 module.exports = { hammingWeight };
