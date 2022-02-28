ad848de4356aeb4eb979ce12b9278062159c15e6
ana scuturici <anascuturici@gmail.com>
2022-02-28 20:56:00 +0900

    add: reverseBits problem

---
+++ 8. reverseBits/reverseBits.js
@@ -0,0 +1,11 @@
+/** BINARY
+ * Reverse bits of a given 32 bits unsigned integer.
+ * 
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+ const reverseBits = n => {
+    
+};
+
+module.exports = { reverseBits };
---
+++ 8. reverseBits/test.js
@@ -0,0 +1,9 @@
+const { expect } = require("chai");
+const { reverseBits } = require("./reverseBits.js");
+
+describe("reverseBits", () => {
+  it("Function produces expected results with valid input.", () => {
+    expect(reverseBits('00000010100101000001111010011100')).to.equal(964176192);
+    expect(reverseBits('11111111111111111111111111111101')).to.equal(3221225471);
+     });
+});
