7fbff93d512cc1843df2686d0a063ef28233baf4
ana scuturici <anascuturici@gmail.com>
2022-02-28 20:49:19 +0900

    delete: .only in test

--- 7.missingNumber/test.js
+++ 7.missingNumber/test.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { missingNumber } = require("./missingNumber.js");
 
-describe.only("missingNumber", () => {
+describe("missingNumber", () => {
   it("Function produces expected results with valid input.", () => {
     expect(missingNumber([3,0,1])).to.equal(2);
     expect(missingNumber([0,1])).to.equal(2);
