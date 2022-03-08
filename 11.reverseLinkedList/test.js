1e7c8392be44815a0a4152f774a42219be151e35
ana scuturici <anascuturici@gmail.com>
2022-03-08 23:04:44 +0900

    refactor: test for reverseLinkedList

--- 11.reverseLinkedList/test.js
+++ 11.reverseLinkedList/test.js
@@ -1,12 +1,23 @@
 const { expect } = require("chai");
 const { reverseList } = require("./reverseList.js");
 
-describe("houseRobber", () => {
+describe("reverseList", () => {
+  const createList = arr => {
+    return arr.reduceRight((next, value) => ({value, next}), null);
+  }
+  
+  let test1 = createList([1,2,3,4,5]);
+  let reversed1 = createList([5,4,3,2,1]);
+  let test2 = createList([1, 2]);
+  let reversed2 = createList([2,1]);
+  let test3 = createList([]);
+  let test4 = createList([]);
+  
   it("Function produces expected results with valid input.", () => {
-    expect(reverseList([1,2,3,4,5])).to.deep.equal([5,4,3,2,1]);
-    expect(reverseList([1, 2])).to.deep.equal([2, 1]);
+    expect(reverseList(test1)).to.deep.equal(reversed1);
+    expect(reverseList(test2)).to.deep.equal(reversed2);
   });
-  it("Should return an empty erray", () => {
-    expect(reverseList([])).to.deep.equal([]);
+  it("Should take an empty erray as input", () => {
+    expect(reverseList(test3)).to.equal(test4);
   });
 });
