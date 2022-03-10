f1422fdd27447a7299547921da62f4142695eca6
ana scuturici <anascuturici@gmail.com>
2022-03-10 22:18:42 +0900

    add: linked list cycle requirements

---
+++ 12.linkedListCycle/test.js
@@ -0,0 +1,35 @@
+const { expect } = require("chai");
+const { listCycle } = require("./listCycle.js");
+
+describe.only("reverseList", () => {
+  class ListNode { 
+    constructor(val, next){
+      this.val = (val===undefined ? 0 : val);
+      this.next = (next===undefined ? null : next);
+    }
+  }
+
+  const createList = arr => {
+    let node, temp;
+    for(let i = arr.length - 1; i >= 0; i--){
+      if(!node){
+        node = new ListNode(arr[i]);
+      } else {
+        temp = new ListNode(arr[i]);
+        temp.next = node;
+        node = temp;
+      }
+    }
+    return node;
+  }
+
+  let test1 = createList([3,2,0,-4]);
+  let test2 = createList([1, 2]);
+  let test3 = createList([1]);
+  
+  it("Function produces expected results with valid input.", () => {
+    expect(listCycle(test1, 1)).to.be.true;
+    expect(listCycle(test2, 0)).to.be.true;
+    expect(listCycle(test3, -1)).to.be.false;
+  });
+});
