c24d302c137ba7513e3bc3a92e270c4412ed4ab2
ana scuturici <anascuturici@gmail.com>
2022-03-13 23:21:59 +0900

    fix: isAnagram test

--- 14.validAnagram/test.js
+++ 14.validAnagram/test.js
@@ -1,10 +1,9 @@
 const { expect } = require("chai");
 const { isAnagram } = require("./isAnagram.js");
 
-describe("twoSum", () => {
+describe("isAnagram", () => {
   it("Function produces expected results with valid input.", () => {
-    expect(isAnagram([2,7,11,15], 9)).to.deep.equal([0, 1]);
-    expect(isAnagram([3,2,4], 6)).to.deep.equal([1, 2]);
-    expect(isAnagram([3,3], 6)).to.deep.equal([0, 1]);
+    expect(isAnagram("anagram", "nagaram")).to.be.true;
+    expect(isAnagram("rat", "car")).to.be.false;
      });
 });
