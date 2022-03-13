55e209bfd6075a41625cc47a6e4578a89d70001c
ana scuturici <anascuturici@gmail.com>
2022-03-13 23:22:44 +0900

    add: isAnagram solution using matching obj

--- 14.validAnagram/isAnagram.js
+++ 14.validAnagram/isAnagram.js
@@ -13,7 +13,27 @@ typically using all the original letters exactly once.
  * @return {boolean}
  */
  const isAnagram = (s, t) => {
-    
+    let obj1 = {};
+    let obj2 = {};
+    let result = false;
+
+    if(s.length !== t.length) return false;
+
+    for(let i = 0; i < s.length; i++){
+        if(Object.keys(obj1).includes(s[i])){
+            obj1[s[i]]++;
+        } else obj1[s[i]] = 1;
+        if(Object.keys(obj2).includes(t[i])){
+            obj2[t[i]]++;
+        } else obj2[t[i]] = 1;
+    }
+
+    for(let i = 0; i < s.length; i++){
+        if(obj1[s[i]] === obj2[s[i]]){
+            result = true;
+        } else return false;
+    }
+    return result;
 };
 
 module.exports = { isAnagram };
