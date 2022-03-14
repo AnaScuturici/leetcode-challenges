74c32da64ebb4501bd7262971afbc54eaaa66e6d
ana scuturici <anascuturici@gmail.com>
2022-03-14 21:38:51 +0900

    add: slow & fast solution for linked list cycle

--- 12.linkedListCycle/listCycle.js
+++ 12.linkedListCycle/listCycle.js
@@ -12,8 +12,19 @@ Otherwise, return false.
  * @param {ListNode} head
  * @return {boolean}
  */
+
+// slow & fast pointers; tortoise & hare solution
  const listCycle = head => {
-    
+    let fast = head;
+    let slow = head;
+
+    while (fast & fast.next){
+        fast = fast.next.next;
+        slow = slow.next;
+        if(fast === slow) 
+            return true;
+    }
+    return false;
 };
 
 module.exports = { listCycle };
