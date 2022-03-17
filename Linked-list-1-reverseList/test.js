f6d93235e730a57951ce6d32a32c4778ecbea0e4
ana scuturici <anascuturici@gmail.com>
2022-03-17 23:35:01 +0900

    update: create linked list function in test

--- Linked-list-1-reverseList/test.js
+++ Linked-list-1-reverseList/test.js
@@ -10,31 +10,26 @@ describe("reverseList", () => {
   }
 
   const createList = arr => {
-    let node, temp;
-    for(let i = arr.length - 1; i >= 0; i--){
-      if(!node){
-        node = new ListNode(arr[i]);
-      } else {
-        temp = new ListNode(arr[i]);
-        temp.next = node;
-        node = temp;
-      }
+    let head = null;
+    for (let i = arr.length - 1; i >= 0; --i) {
+      let newHead = new ListNode(arr[i]);
+      newHead.next = head;
+      head = newHead;
     }
-    return node;
-  }
+    return head;
+  };
 
   let test1 = createList([1,2,3,4,5]);
   let reversed1 = createList([5,4,3,2,1]);
   let test2 = createList([1, 2]);
   let reversed2 = createList([2,1]);
-  //let test3 = createList([]);
+  let test3 = createList([]);
   
   it("Function produces expected results with valid input.", () => {
     expect(reverseList(test1)).to.deep.equal(reversed1);
     expect(reverseList(test2)).to.deep.equal(reversed2);
   });
-  // NEED to UPDATE
-  // it("Should take an empty erray as input", () => {
-  //   expect(reverseList(test3)).to.be.null;
-  // });
+  it("Should take an empty erray as input", () => {
+    expect(reverseList(test3)).to.be.null;
+  });
 });
