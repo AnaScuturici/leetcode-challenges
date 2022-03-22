b0eac4a8e7f325b98dbcca10a32eed239e31de35
ana scuturici <anascuturici@gmail.com>
2022-03-22 22:48:18 +0900

    add: recursive solution for invert tree

--- Tree-3-invertTree/invertTree.js
+++ Tree-3-invertTree/invertTree.js
@@ -5,7 +5,16 @@
  * @return {TreeNode}
  */
  const invertTree = root => {
-    
+    if(root === null || root.left === null && root.right === null){
+        return root;
+    }
+
+    const temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+    invertTree(root.left);
+    invertTree(root.right);
+    return root;
 };
 
 module.exports = { invertTree };
