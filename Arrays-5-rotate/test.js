03a68076e64cfe7ac4331d39554f82d42bc513d6
ana scuturici <anascuturici@gmail.com>
2022-05-06 17:01:03 +0900

    update: rotate array test

--- Arrays-5-rotate/test.js
+++ Arrays-5-rotate/test.js
@@ -1,8 +1,10 @@
 const { expect } = require("chai");
 const { rotate } = require("./rotate.js");
 
-describe("rotate array", () => {
+describe.only("rotate array", () => {
   it("Function produces expected results with valid input.", () => {
     expect(rotate([1,2,3,4,5], 2)).to.deep.equal([4,5,1,2,3]);
+    expect(rotate([1,2], 7)).to.deep.equal([2,1]);
+    expect(rotate([], 3)).to.deep.equal([]);
      });
 });
