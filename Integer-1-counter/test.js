8f2454b1cf827aba5bfc1314f1e1f1f521e7472d
ana scuturici <anascuturici@gmail.com>
2023-05-15 18:04:38 +0900

    add counter function

---
+++ Integer-1-counter/test.js
@@ -0,0 +1,19 @@
+const { expect } = require("chai");
+const { createCounter } = require("./counter.js");
+
+describe.only("counter", () => {
+    it("should produce expected results with valid input", () => {
+        const counter = createCounter(10);
+        console.log(counter);
+        expect(counter()).to.equal(10);
+        expect(counter()).to.equal(11);
+        expect(counter()).to.equal(12);
+    });
+    it("should work with negative numbers", () => {
+        const counter = createCounter(-2);
+        expect(counter()).to.equal(-2);
+        expect(counter()).to.equal(-1);
+        expect(counter()).to.equal(0); 
+        expect(counter()).to.equal(1);
+    });
+});
