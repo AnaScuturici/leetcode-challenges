ef4a37f154583f8c007769debe2584eede44c9ac
ana scuturici <anascuturici@gmail.com>
2023-05-15 17:10:23 +0900

    delete .only

--- Arrays-5-rotate/test.js
+++ Arrays-5-rotate/test.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { rotate } = require("./rotate.js");
 
-describe.only("rotate array", () => {
+describe("rotate array", () => {
   it("Function produces expected results with valid input.", () => {
     expect(rotate([1,2,3,4,5], 2)).to.deep.equal([4,5,1,2,3]);
     expect(rotate([1,2], 7)).to.deep.equal([2,1]);
